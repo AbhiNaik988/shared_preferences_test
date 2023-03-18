e67fdfeb1d8efa9df564fc4e65147fc01dcbe2e7
Abhi Naik <83689490+AbhiNaik988@users.noreply.github.com>
2023-03-18 23:38:58 +0530

    updated debugPring in bloc Observer file

--- lib/blocs/observer.dart
+++ lib/blocs/observer.dart
@@ -5,38 +5,38 @@ class MyGlobalObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    debugPrint(bloc.toString());
+    debugPrint("bloc: onCreate: bloc: ${bloc.toString()}");
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    debugPrint(transition.toString());
+    debugPrint("bloc: onTransition: transition: ${transition.toString()}");
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    debugPrint(change.toString());
-    debugPrint(change.currentState.toString());
-    debugPrint(change.nextState.toString());
+    debugPrint("bloc: onChange: change: ${change.toString()}");
+    debugPrint("bloc: onChange: currentState: ${change.currentState.toString()}");
+    debugPrint("bloc: onChange: nextState: ${change.nextState.toString()}");
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
-    debugPrint(error.toString());
+    debugPrint("bloc: onError: error: ${error.toString()}");
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    debugPrint(event.toString());
+    debugPrint("bloc: onEvent: event: ${event.toString()}");
   }
 
   @override
   void onClose(BlocBase bloc) {
     super.onClose(bloc);
-    debugPrint(bloc.toString());
+    debugPrint("bloc: onClose: bloc: ${bloc.toString()}");
   }
 }
